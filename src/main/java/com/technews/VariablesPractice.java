8eac3639756ee2ed76464f6b0a500de52daa5128
Javarrisb <javarrisb@gmail.com>
2022-09-16 14:23:11 -0400

    created relationships for user table

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,21 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+       int a = 5;
+       double b = 4.5;
+       String c = "cat";
+
+       // calculated variable sum is a double
+        double sum = a + b;
+
+        // calculated variable concat is a string
+        String concat = a + c;
+
+        // value of sum is 9.5
+        System.out.println(sum);
+
+        // value of concat is "5cat"
+        System.out.println(concat);
+    }
+}
